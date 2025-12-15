825b6e97e15d335ec36b284e82cd7a8fe8257393
Muhammed Ansif <mohdansif05@gmail.com>
2025-12-15 20:26:01 +0530

    Fix CORS without wildcard options

--- server.js
+++ server.js
@@ -17,12 +17,25 @@ const userRoutes = require("./src/api/users/user.routes");
 
 
 //middleware
-app.use(cors({
-  origin: "*",                     // allow all origins (for development)
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type, Authorization",
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "*",                     // allow all origins (for development)
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   allowedHeaders: "Content-Type, Authorization",
+//   credentials: true
+// }));
+
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+
+
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
