7727c7402ad4b1ec750d986aadad812366a12085
Muhammed Ansif <mohdansif05@gmail.com>
2025-12-16 11:13:56 +0530

    fix server config

--- server.js
+++ server.js
@@ -7,46 +7,34 @@ const authRoutes = require("./src/api/auth/auth.routes");
 const productRoutes = require("./src/api/products/products.routes");
 const customerRoutes = require("./src/api/customers/customers.routes");
 const saleRoutes = require("./src/api/sales/sales.routes");
-const reportRoutes =require("./src/api/reports/reports.routes");
+const reportRoutes = require("./src/api/reports/reports.routes");
 const invoiceRoutes = require("./src/api/printing/invoice.routes");
 const paymentRoutes = require("./src/api/payments/payments.routes");
 const dashboardRoutes = require("./src/api/dashboard/dashboard.routes");
-const stockRoutes =require("./src/api/stock/stock.routes");
+const stockRoutes = require("./src/api/stock/stock.routes");
 const uploadProductImage = require("./src/middleware/uploadProductImage");
 const userRoutes = require("./src/api/users/user.routes");
 
-
 //middleware
-// app.use(cors({
-//   origin: "*",                     // allow all origins (for development)
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   allowedHeaders: "Content-Type, Authorization",
-//   credentials: true
-// }));
-
-
-app.use(cors({
-  origin: true,               // 👈 allow the requesting origin dynamically
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
-
-
-
-
-
+app.use(
+  cors({
+    origin: "*", // allow all origins (for development)
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/customers",customerRoutes);
-app.use("/api/sales",saleRoutes);
-app.use("/api/reports",reportRoutes);
-app.use("/api/invoices",invoiceRoutes);
-app.use("/api/payments",paymentRoutes);
-app.use("/api/dashboard",dashboardRoutes);
-app.use("/api/stock",stockRoutes);
+app.use("/api/customers", customerRoutes);
+app.use("/api/sales", saleRoutes);
+app.use("/api/reports", reportRoutes);
+app.use("/api/invoices", invoiceRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/stock", stockRoutes);
 app.use("/api/users", userRoutes);
 // Serve uploaded files
 app.use("/uploads", express.static("uploads"));
